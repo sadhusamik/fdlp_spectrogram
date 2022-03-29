d22890586e5b7a2eb28af71e236c95c21156319b
Kocour Martin <ikocour@fit.vutbr.cz>
2022-03-29 14:58:22 +0200

    Fix issues with feature extractions

--- fdlp/bin/cli.py
+++ fdlp/bin/cli.py
@@ -1,4 +1,5 @@
 import click
+import sys
 from fdlp import FDLP
 
 @click.group()
@@ -48,15 +49,18 @@ def kaldi(wav_scp, wspecifier, n_filters, coeff_num, coeff_range, order, fdurati
     import kaldi_io
     import numpy as np
 
-    click.echo(f"make-kaldi-fdlp {wav_scp} {wspecifier}")
+    click.echo(f"make-fdlp kaldi {wav_scp} {wspecifier}")
     fea_extractor = FDLP(n_filters, coeff_num, coeff_range, order,
         fduration, frate, overlap_fraction, srate=srate)
     click.echo(vars(fea_extractor))
 
     feat_lens = {}
+    err_cnt = 0
+    cnt = 0
     with open(wav_scp, "r") as fd_in,\
          kaldi_io.open_or_fd(wspecifier, "wb") as fd_out:
         for line in fd_in:
+            cnt += 1
             utt, audio_f = line.split()
             offset = 0
             if len(audio_f.split(":")) == 2:
@@ -68,14 +72,22 @@ def kaldi(wav_scp, wspecifier, n_filters, coeff_num, coeff_range, order, fdurati
                 x, sr = sf.read(f)
 
             x = np.expand_dims(x, axis=0)
-            fea, fea_len = fea_extractor.extract_feats(x, ilens=None)
+            try:
+                fea, fea_len = fea_extractor.extract_feats(x, ilens=None)
+            except Exception as e:
+                err_cnt += 1
+                print("WARNING: Failed to extract feats from utt \"" + utt + "\" :" + str(e), file=sys.stderr)
+                continue
+
             kaldi_io.write_mat(fd_out, np.squeeze(fea, axis=0), key=utt)
             feat_lens[utt] = fea_len
     
     if utt2num_frames is not None:
         with open(utt2num_frames, "w") as fd:
-            for utt, fea_len in fea_lens.items():
+            for utt, fea_len in feat_lens.items():
                 fd.write(f"{utt} {fea_len}\n")
+
+    print(f"Failed to extract FDLP features in {err_cnt}/{cnt} cases.", file=sys.stderr)
 
 if __name__ == '__main__':
     cli()
